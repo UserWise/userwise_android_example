300be5040dacb6be290dd1b5de24880e639a143e
David Jenkins <david@theoremreach.com>
2020-05-12 11:38:02 -0500

    Add survey invite dialog for a better example representation.

--- app/src/main/java/io/userwise/userwise_android_example/MainActivity.java
+++ app/src/main/java/io/userwise/userwise_android_example/MainActivity.java
@@ -3,9 +3,11 @@ package io.userwise.userwise_android_example;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
     private static Logger logger = Logger.getLogger("userwise_example_app");
 
     private UserWise userWise = UserWise.INSTANCE;
+    private Dialog surveyOffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,8 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
         logger.info("UserWise Survey Listener Set");
 
         userWise.setContext(this);
-        userWise.setUserId("userwise-android-example");
-        userWise.setApiKey("fe6dc94555e366a36be767d6d937");
+        userWise.setUserId("userwise-android-exampleee");
+        userWise.setApiKey("2fac619fdeecba9f3fb3c7228406");
         // or: userWise.initialize(context, apiKey, userId);
 
         // You can also update the styles of the loading screen. Uncomment to see the example app's
@@ -45,6 +48,15 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
         //Drawable logo = ContextCompat.getDrawable(this, R.drawable.userwise_herowars_logo);
         //userWise.setSplashScreenLogo(logo);
 
+
+        //try {
+        //JSONObject attributes = new JSONObject().put("favorite_color", "blue");
+
+        //userWise.assignEvent("event_logged_in", attributes);
+        //userWise.setAttributes(attributes);
+        //} catch (JSONException e) {}
+
+
         logger.info("API Key and User ID Set");
     }
 
@@ -52,25 +64,44 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
         userWise.refreshHasAvailableSurveys();
     }
 
-    public void takeNextSurvey(View view) {
-        //try {
-            //JSONObject attributes = new JSONObject().put("favorite_color", "blue");
-
-            //userWise.assignEvent("event_logged_in", attributes);
-            //userWise.setAttributes(attributes);
-        //} catch (JSONException e) {}
-
-
+    public void initializeSurveyInvite() {
         userWise.initializeSurveyInvite(this);
+    }
+
+    public void declineSurveyInvite(View view) {
+        this.dismissSurveyOffer();
+        userWise.setSurveyInviteResponse(false);
+    }
+
+    public void acceptSurveyInvite(View view) {
+        this.dismissSurveyOffer();
+        userWise.setSurveyInviteResponse(true);
+    }
+
+    private void dismissSurveyOffer() {
+        if (this.surveyOffer != null) {
+            this.surveyOffer.dismiss();
+        }
+    }
+
+    private void showSurveyOffer() {
+        if (this.surveyOffer == null) {
+            this.surveyOffer = new Dialog(this);
+            this.surveyOffer.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            this.surveyOffer.getWindow().setLayout(414, 736);
+            this.surveyOffer.setContentView(getLayoutInflater().inflate(R.layout.survey_invite_layout,null));
+        }
+
+        this.surveyOffer.show();
     }
 
     @Override
     public void onSurveyInviteInitialized(Boolean wasInitialized) {
         // UserWise failed to start the survey initialization process. Don't show the survey invite
         // dialog to the user.
-        if (!wasInitialized) { return; }
+        if (!wasInitialized || userWise.isTakingSurvey()) { return; }
 
-        userWise.setSurveyInviteResponse(true);
+        this.showSurveyOffer();
     }
 
     @Override
@@ -81,15 +112,12 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
     @Override
     public void onSurveyAvailable() {
         if (userWise.isTakingSurvey()) { return; }
-
-        // Called when surveys are available for the appuser
-        Toast.makeText(this, "Surveys are available!", Toast.LENGTH_SHORT).show();
+        this.initializeSurveyInvite();
     }
 
     @Override
     public void onSurveysUnavailable() {
-        // Called when no surveys are available for the appuser
-        Toast.makeText(this, "No surveys are available to take.", Toast.LENGTH_SHORT).show();
+        // Called when no surveys are available for the app user
     }
 
     @Override
@@ -100,14 +128,14 @@ public class MainActivity extends AppCompatActivity implements UserWiseSurveyLis
     }
 
     @Override
-    public void onSurveyCompleted() {
-        // Called when a survey has been successfully completed
-        Toast.makeText(this, "Survey was successfully completed!", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
     public void onSurveyEnterFailed() {
         // Called when a survey was unable to properly be loaded
         Toast.makeText(this, "Survey failed to load!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSurveyCompleted() {
+        // Called when a survey has been successfully completed
+        Toast.makeText(this, "Survey was successfully completed!", Toast.LENGTH_SHORT).show();
     }
 }
