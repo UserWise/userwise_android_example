44d8a95e883bd5411e31a16d52f96a8ac981b68b
David Jenkins <david@theoremreach.com>
2021-03-05 16:54:27 -0500

    Update to v2.0.1

--- app/src/main/java/io/userwise/userwise_android_example/ExampleOfferHandler.java
+++ app/src/main/java/io/userwise/userwise_android_example/ExampleOfferHandler.java
@@ -42,55 +42,23 @@ public class ExampleOfferHandler implements OfferEventListener {
     @Override
     public void onOfferImpressionInitialized(@NotNull OfferImpression offerImpression) {
         Log.d(TAG, "Offer impression initialized! Offer impression id " + offerImpression.getId());
-        UserWise.INSTANCE.getOffers().showOffer(offerImpression);
-        // Above, we tell the UserWise SDK to show the offer, as it was built in our dashboard.
-        // However, if you find yourself wanting a data-only approach, you can hook into-and out of-
-        // the UserWise OffersModule event flow, at any point.
 
-        // Offer impressions contain information about their `offer`, including bundle information
-        // (e.g. pricing, contents, etc.).
-
-        // Offer impressions can have their state updated by calling  offerImpression.updateState();
-    }
-
-    @Override
-    public void onOfferViewAttemptFailed(@NotNull OfferImpression offerImpression, @NotNull OfferViewAttemptFailedReason reason) {
-        Log.d(TAG, "Offer failed to load properly. Reason: " + reason.toString());
-    }
-
-    @Override
-    public void onOfferViewed(@NotNull OfferImpression offerImpression) {
-        Log.d(TAG, "Offer has loaded and is actively visible!");
-    }
-
-    @Override
-    public void onOfferDismissed(@NotNull OfferImpression offerImpression) {
-        Log.d(TAG, "Offer was dismissed!");
-    }
-
-    @Override
-    public void onOfferAccepted(@NotNull OfferImpression offerImpression) {
-        Log.d(TAG, "Offer was accepted!");
 
         Offer offer = offerImpression.getOffer();
 
         String productId = offer.getAndroidProductId();
         Double cost = offer.getCost();
-        JSONObject bundleContent = offer.getCurrencies();
+        JSONObject bundledCurrencies = offer.getCurrencies();
+        JSONObject bundledItems = offer.getItems();
 
-        Iterator<String> it = bundleContent.keys();
-        StringBuilder str = new StringBuilder();
-        while (it.hasNext()) {
-            String currencyId = it.next();
-            int currencyAmount = bundleContent.optInt(currencyId, 0);
-
-            str.append("   * ").append(currencyAmount).append(currencyId).append("\n");
-        }
-
-        Log.d(TAG, "Offer Name: " + offer.getName());
-        Log.d(TAG, "Offer Product Id: " + productId);
+        Log.d(TAG, "Offer Title: " + offer.getTitle());
+        Log.d(TAG, "Offer Body: " + offer.getBody());
+        Log.d(TAG, "Offer Portrait Image ID: " + offer.getPortraitImageId());
+        Log.d(TAG, "Offer Landscape Image ID: " + offer.getLandscapeImageId());
+        Log.d(TAG, "Offer Product ID: " + productId);
         Log.d(TAG, "Offer Cost: " + cost.toString());
-        Log.d(TAG, "Bundle Contains: \n" + str.toString());
+        Log.d(TAG, "Offer Bundled Currencies: \n" + bundledCurrencies.toString());
+        Log.d(TAG, "Offer Bundled Items: \n" + bundledItems.toString());
 
         // Once an offer has been accepted the itself impression will stay in a state of "accepted"
         // on the UserWise servers.  There are currently two possible states beyond "accepted":
@@ -107,5 +75,15 @@ public class ExampleOfferHandler implements OfferEventListener {
         // 2. You display the buy screen (or maybe that itself fails). User does not purchase. You call:
         //offerImpression.updateState(OfferImpressionState.PURCHASE_FAILED);
     }
+
+    // these are all ignored
+    @Override
+    public void onOfferViewAttemptFailed(@NotNull OfferImpression offerImpression, @NotNull OfferViewAttemptFailedReason reason) {}
+    @Override
+    public void onOfferViewed(@NotNull OfferImpression offerImpression) {}
+    @Override
+    public void onOfferDismissed(@NotNull OfferImpression offerImpression) {}
+    @Override
+    public void onOfferAccepted(@NotNull OfferImpression offerImpression) {}
 
 }
